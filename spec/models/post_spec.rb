878205c5c2aca7cf4ff474ca7a8c23b724db8541
Ashish Mainali <ashishmainali@bajratechnologies.com>
2022-02-14 17:59:17 +0545

    feature : Added specs for post content

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,11 +2,19 @@ require 'rails_helper'
 
 MIN_TITLE_LENGTH = 3
 MAX_TITLE_LENGTH = 100
+MIN_CONTENT_LENGTH = 10
+MAX_CONTENT_LENGTH = 400
+
 RSpec.describe Api::V1::Post, type: :model do
   context 'it should validate the post title' do
     it 'is not valid without post title' do
       post = build(:api_v1_post, title: nil)
       expect(post).to_not be_valid
+    end
+
+    it 'is valid with post title' do
+      post = build(:api_v1_post)
+      expect(post).to be_valid
     end
 
     it "is not valid with post title less than #{MIN_TITLE_LENGTH}" do
@@ -25,6 +33,30 @@ RSpec.describe Api::V1::Post, type: :model do
       title = "I am a sample title #{rand.to_s[2..9]} "
       create(:api_v1_post, title: title)
       expect { create(:api_v1_post, title: title) }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
+
+  context 'it should validate the post content' do
+    it 'is not valid without post content' do
+      post = build(:api_v1_post, content: nil)
+      expect(post).to_not be_valid
+    end
+
+    it 'is valid with post content' do
+      post = build(:api_v1_post)
+      expect(post).to be_valid
+    end
+
+    it "is not valid with post content less than #{MIN_CONTENT_LENGTH}" do
+      content = 'a' * 5
+      post_one = build(:api_v1_post, content: content)
+      expect(post_one).to_not be_valid
+    end
+
+    it "is not valid with post content greater than #{MAX_CONTENT_LENGTH}" do
+      content = 'a' * 500
+      post_one = build(:api_v1_post, content: content)
+      expect(post_one).to_not be_valid
     end
   end
 end
