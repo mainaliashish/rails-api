ad12f1f8f27ebb6dcd216c5fbe6e9ec18c7f4d0e
Ashish Mainali <ashishmainali@bajratechnologies.com>
2022-02-15 18:03:17 +0545

    fix: minor indentation fixes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe User, type: :model do
   context 'it should validate the user email address' do
     it 'is not valid without email address' do
       user = build(:user, email: nil)
-      expect(user).to_not be_valid  
+      expect(user).to_not be_valid
     end
 
     it 'is not valid without invalid email format' do
@@ -21,21 +21,21 @@ RSpec.describe User, type: :model do
       email = "ashish#{number}@yahoo.com"
       # binding.break
       create(:user, email: email)
-      expect { create(:user, email: email) }.to raise_error(ActiveRecord::RecordInvalid)  
+      expect { create(:user, email: email) }.to raise_error(ActiveRecord::RecordInvalid)
     end
 
     it "must have a valid email length of #{MAX_EMAIL_LENGTH}" do
-      prefix = "abc" * 106
+      prefix = 'abc' * 106
       email = "#{prefix}@gmail.com"
       user = build(:user, email: email)
-      expect(user).to_not be_valid  
+      expect(user).to_not be_valid
     end
   end
 
   context 'should validate the user password' do
     it 'is not valid without password' do
       user = build(:user, password: nil)
-      expect(user).to_not be_valid  
+      expect(user).to_not be_valid
     end
 
     it "should have minimum password length of #{MIN_PASSWORD_LENGTH}" do
@@ -47,12 +47,12 @@ RSpec.describe User, type: :model do
     it "should have maximum password length of #{MAX_PASSWORD_LENGTH}" do
       password = 'a' * 51
       user = build(:user, password: password)
-      expect(user).to_not be_valid  
+      expect(user).to_not be_valid
     end
   end
 
   it 'is valid with a valid email address and password' do
     user = build(:user)
-    expect(user).to be_valid  
+    expect(user).to be_valid
   end
 end
